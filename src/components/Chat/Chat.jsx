c0c05c3702f70b2f7547445b3a99415691f9c7f6
Yug Sharma <yugsharma422@gmail.com>
2022-08-09 18:44:44 +0530

    Chat-app completed without responsive

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { user } from "../Join/Join";
 import socketIO from "socket.io-client";
 import "./Chat.css";
 import sendLogo from "../../images/send-logo.png";
+import Message from "../Message/Message";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import close from "../../images/close.jpg";
 
 const ENDPOINT = "http://localhost:4500";
 let socket;
 const Chat = () => {
   socket = socketIO(ENDPOINT, { transports: ["websocket"] });
   const [id, setId] = useState("");
+  const [messages, setMessages] = useState([]);
 
   //Sending message
   const send = () => {
@@ -28,14 +32,17 @@ const Chat = () => {
     socket.emit("joined", { user });
 
     socket.on("welcome", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
     });
 
     socket.on("userJoined", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
     });
 
     socket.on("leave", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
     });
 
@@ -47,15 +54,34 @@ const Chat = () => {
 
   useEffect(() => {
     socket.on("sendMessage", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message, data.id);
     });
-  }, []);
+
+    return () => {
+      socket.off();
+    };
+  }, [messages]);
 
   return (
     <div className="chatPage">
       <div className="chatContainer">
-        <div className="header"></div>
-        <div className="chatBox"></div>
+        <div className="header">
+          <h2>Textra</h2>
+          <a href="/" className="closeBtn">
+            <img src={close} alt="close" />
+          </a>
+        </div>
+        <ReactScrollToBottom className="chatBox">
+          {messages.map((item, i) => (
+            <Message
+              key={i}
+              user={item.id == id ? "" : item.user}
+              message={item.message}
+              classs={item.id === id ? "right" : "left"}
+            />
+          ))}
+        </ReactScrollToBottom>
         <div className="inputBox">
           <input type="text" id="chatInput" />
           <button onClick={send} className="sendBtn">
